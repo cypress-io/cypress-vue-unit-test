71eddfd4e0c8d1329849a49b7c0f29997fa3f58b
Amir Rustamzadeh <amirrustam@gmail.com>
2018-01-26 13:17:16 -0800

    feat: added example tests for Counter component
    Added tests to cover all the features within the component. Fixes bahmutov/cypress-vue-unit-test#6

--- cypress/integration/counter-vuex-spec.js
+++ cypress/integration/counter-vuex-spec.js
@@ -2,11 +2,13 @@
 // https://github.com/vuejs/vuex/tree/dev/examples/counter
 import Counter from '../../components/Counter.vue'
 import store from '../../components/store'
-const mountVue = require('../..')
+import Vuex from 'vuex'
+import mountVue from '../..'
 
 /* eslint-env mocha */
 describe('Vuex Counter', () => {
-  const extensions = {    
+  const extensions = {
+    plugins: [Vuex],
     components: {
       counter: Counter
     },
@@ -14,7 +16,37 @@ describe('Vuex Counter', () => {
   const template = '<counter />'
   beforeEach(mountVue({template, store}, {extensions}))
 
+  const getCount = () =>
+    Cypress.vue.$store.state.count
+
+  const setCount = value => {
+    Cypress.vue.$store.state.count = value
+  }
+
   it('starts with zero', () => {
-    cy.contains('button', '0')
+    cy.contains('0 times')
+  })
+
+  it('increments the counter on click of "+"', () => {
+    cy.contains('button', '+').click()
+    cy.contains('1 times')
+  })
+
+  it('decrements the counter on click of "-"', () => {
+    cy.contains('button', '-').click()
+    cy.contains('0 times')
+  })
+
+  it('increments the counter if count is odd', () => {
+    setCount(3)
+    cy.contains('odd')
+    cy.contains('button', 'Increment if odd').click()
+    cy.contains('even')
+  })
+
+  it('asynchronously increments counter', () => {
+    let count = getCount()
+    cy.contains('button', 'Increment async').click()
+    cy.contains(`${count++} times`)
   })
 })
