b00b876c54b5e395ff01831881965dc333bf3692
Nick Bolles <me@nickbolles.com>
2019-12-19 23:19:19 -0600

    feat: add typescript typings

---
+++ index.d.ts
@@ -0,0 +1,32 @@
+import Vue, { ComponentOptions } from "vue";
+
+declare global {
+  namespace Cypress {
+    import Vue from "vue";
+
+    interface Cypress {
+      vue: Vue;
+    }
+  }
+}
+
+interface Options {
+  mountId?: string;
+  html?: string;
+  base?: string;
+  extensions: {
+    components: any[];
+    filters: any[];
+    use: any[];
+    plugins: any[];
+    mixin: any[];
+    mixins: any[];
+  };
+}
+
+function mountVue<V extends Vue>(
+  component: Vue,
+  optionsOrProps?: Options
+): () => void;
+
+export = mountVue;
