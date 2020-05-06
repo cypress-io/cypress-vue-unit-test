5907b85d2819f72580b5e287032bb7464b59e287
Ben Hong <ben@bencodezen.io>
2020-05-06 14:41:24 -0400

    test: add vue3 temp spec

---
+++ cypress/component/button/button-vue3.spec.js
@@ -0,0 +1,23 @@
+import ButtonCounter from './ButtonCounter.vue'
+import { mountCallback } from '../../../src/vue3/index'
+
+/* eslint-env mocha */
+describe('ButtonCounter', () => {
+  beforeEach(mountCallback(ButtonCounter))
+
+  it('starts with zero', () => {
+    cy.contains('button', '0')
+  })
+
+  it('increments the counter on click', () => {
+    cy.get('button').click().click().click().contains('3')
+  })
+
+  it('emits "increment" event on click', () => {
+    const spy = cy.spy()
+    Cypress.vue.$on('increment', spy)
+    cy.get('button').click().click().then(() => {
+      expect(spy).to.be.calledTwice
+    })
+  })
+})
