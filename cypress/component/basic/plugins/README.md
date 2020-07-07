d0078c7eea83bedaa5d5ea6a08f14082f9be09a0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-07-07 10:27:45 -0400

    move plugin example into own folder

---
+++ cypress/component/basic/plugins/README.md
@@ -0,0 +1,3 @@
+# plugins
+
+Spec file [plugin-spec.js](plugin-spec.js) shows how to test a component while loading additional plugins.
--- cypress/component/basic/plugins/plugin-spec.js
+++ cypress/component/basic/plugins/plugin-spec.js
@@ -1,7 +1,22 @@
+/// <reference types="cypress" />
 import { MyPlugin } from './MyPlugin'
-import { mountCallback } from 'cypress-vue-unit-test'
+import { mount, mountCallback } from 'cypress-vue-unit-test'
 
-/* eslint-env mocha */
+describe('Single component mount', () => {
+  it('has the plugin', () => {
+    const use = [MyPlugin]
+
+    // extend Vue with plugins
+    const extensions = {
+      use,
+    }
+
+    mount({}, { extensions })
+
+    cy.window().its('Vue').invoke('aPluginMethod').should('equal', 'foo')
+  })
+})
+
 describe('Custom plugin MyPlugin', () => {
   const use = [MyPlugin]
 
@@ -9,6 +24,7 @@ describe('Custom plugin MyPlugin', () => {
   const extensions = {
     use,
   }
+  // use "mountCallback" to register the plugins
   beforeEach(mountCallback({}, { extensions }))
 
   it('registers global method on Vue instance', () => {
