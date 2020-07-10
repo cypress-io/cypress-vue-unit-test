0ac2bed0faf4657cb6f9cfbd152ec6d16a95a54c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-07-10 14:02:00 -0400

    add types for Cypress.vue property

--- src/index.ts
+++ src/index.ts
@@ -286,6 +286,28 @@ interface MountOptions {
  */
 type MountOptionsArgument = Partial<ComponentOptions & MountOptions>
 
+// when we mount a Vue component, we add it to the global Cypress object
+// so here we extend the global Cypress namespace and its Cypress interface
+declare global {
+  // eslint-disable-next-line no-redeclare
+  namespace Cypress {
+    interface Cypress {
+      /**
+       * Mounted Vue instance is available under Cypress.vue
+       * @memberof Cypress
+       * @example
+       *  mount(Greeting)
+       *  .then(() => {
+       *    Cypress.vue.message = 'Hello There'
+       *  })
+       *  // new message is displayed
+       *  cy.contains('Hello There').should('be.visible')
+       */
+      vue: Vue
+    }
+  }
+}
+
 /**
  * Direct Vue errors to the top error handler
  * where they will fail Cypress test
